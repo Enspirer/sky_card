cb8ec0aea06338bcf7d6879b5081c8ed5a98bd89
Jude Lakshitha <judelakshitha@gmail.com>
2021-04-22 22:09:19 +0530

    SignUp/Login Layout development Done

--- SkycardApp/app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ SkycardApp/app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        buttonLogin = (Button) findViewById(R.id.btnLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
