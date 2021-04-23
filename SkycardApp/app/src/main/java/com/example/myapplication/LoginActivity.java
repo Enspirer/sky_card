af3601532f302ece38155ee952f95173309ea30f
Jude Lakshitha <judelakshitha@gmail.com>
2021-04-23 14:39:05 +0530

    Login SignUp API call done

--- SkycardApp/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ SkycardApp/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -3,29 +3,133 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button buttonSignUp;
+
+    Button buttonLogin, buttonSignUp;
+
+    EditText etPass, ete_mail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        buttonSignUp = (Button) findViewById(R.id.btnSignUp);
+        etPass = findViewById(R.id.etPassword);
+        ete_mail = findViewById(R.id.etEmail);
+
+        buttonSignUp = findViewById(R.id.btnSignUp);
+        buttonLogin = findViewById(R.id.btnLogin);
+
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSignUpActivity();
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                requestLogin();
             }
         });
     }
-    public void openSignUpActivity(){
-        Intent intent = new Intent(this,SignUpActivity.class);
-        startActivity(intent);
+
+    private void requestLogin() {
+
+        String url = "http://thechaptersrilanka.com/sky_card_backend/public/api/auth/login";
+        JSONObject obj = new JSONObject();
+
+        try {
+
+            obj.put("password", etPass.getText().toString());
+            obj.put("email", ete_mail.getText().toString());
+            obj.put("remember_me", 1);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(LoginActivity.this, "Internet Connection Failed",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        Log.d("Send Login Request", "login request " + obj);
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                url, obj, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Response", response.toString());
+
+                SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(
+                        "MyPref", MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+
+                try {
+                    //store the key and its values
+                    editor.putString("token", response.getString("response"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                editor.commit();
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+
+                Log.d("Error", "error " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+//            @Override
+//            public Map<String, String> getHeaders() throws AuthFailureError {
+//                Map<String, String> params = new HashMap<String, String>();
+//
+//                SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+//                String token = pref.getString("token","");
+//                params.put("Authorization", "Bearer "+token);
+//                return params;
+//            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjReq);
+
     }
+
+
 }
--- SkycardApp/app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ SkycardApp/app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -3,29 +3,116 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    Button buttonLogin;
+    EditText userName, userEmail, userPassword;
+    Button buttonLogin, buttonSignup;
+
+    String name, password, email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        buttonLogin = (Button) findViewById(R.id.btnLogin);
+        userName = findViewById(R.id.etName);
+        userEmail = findViewById(R.id.etEmail);
+        userPassword = findViewById(R.id.etPassword);
+
+        buttonLogin = findViewById(R.id.btnLogin);
+        buttonSignup = findViewById(R.id.btnSignUp);
+
+        name = userName.getText().toString();
+        password = userPassword.getText().toString();
+        email = userEmail.getText().toString();
+
+//
+//        if (name.matches("")){
+//            Toast.makeText(this,"Enter your name",Toast.LENGTH_SHORT).show();
+//        }
+//        if (password.matches("")){
+//            Toast.makeText(this,"Enter the password",Toast.LENGTH_SHORT).show();
+//        }
+//        if (email.matches("")){
+//            Toast.makeText(this,"Enter your email address",Toast.LENGTH_SHORT).show();
+//        }
+
+
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openLoginActivity();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendData();
             }
         });
     }
-    public void openLoginActivity(){
-        Intent intent = new Intent(this,LoginActivity.class);
-        startActivity(intent);
+
+    private void sendData() {
+
+        String url = "http://thechaptersrilanka.com/sky_card_backend/public/api/auth/signup";
+        JSONObject obj = new JSONObject();
+
+        try {
+            obj.put("first_name", userName.getText().toString());
+            obj.put("last_name","MyLastName");
+            obj.put("password", userPassword.getText().toString());
+            obj.put("email", userEmail.getText().toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(SignUpActivity.this, "Network connection failed",
+                    Toast.LENGTH_SHORT).show();
+        }
+        Log.d("Send data", "send " + obj);
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                url, obj, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Response", response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error", "error " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+
+
+            }
+        });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjReq);
     }
+
 }
