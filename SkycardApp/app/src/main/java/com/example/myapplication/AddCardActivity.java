24f4b164f67d26ca8508e2ed5a385256efc75b86
Jude Lakshitha <judelakshitha@gmail.com>
2021-05-07 01:55:54 +0530

    Start My Card Function

--- SkycardApp/app/src/main/java/com/example/myapplication/AddCardActivity.java
+++ SkycardApp/app/src/main/java/com/example/myapplication/AddCardActivity.java
@@ -1,24 +1,22 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.SurfaceControl;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -27,24 +25,17 @@ import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.ServerError;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.myapplication.helper.AppProgressDialog;
-import com.google.gson.JsonArray;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
+import java.sql.Time;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -52,13 +43,14 @@ public class AddCardActivity extends AppCompatActivity {
 
     ImageView iViewAddCard, ivCloseCard, ivbackCard;
     String base64file;
-
+    Integer card_type;
     Button buttonSaveCard, buttonReScanCard;
+    View popUpWindow;
 
-    EditText etName, etTitle, etcompanyName, etmobileNumber, etotherMobileNumber, etEmail,
+    EditText etUserName, etTitle, etcompanyName, etmobileNumber, etotherMobileNumber, etEmail,
             etwebSite,
             etaddressLine1,
-            etadressLine2;
+            etaddressLine2;
 
     private static final int CONTACT_PERMISSION_CODE = 100;
 
@@ -71,7 +63,8 @@ public class AddCardActivity extends AppCompatActivity {
         iViewAddCard.setImageURI(Uri.fromFile(CameraActivity.imageFile));
         ivbackCard = findViewById(R.id.ivbackCard);
         ivCloseCard = findViewById(R.id.ivCloseCard);
-        etName = findViewById(R.id.etName);
+
+        etUserName = findViewById(R.id.etName);
         etTitle = findViewById(R.id.etTitle);
         etcompanyName = findViewById(R.id.etCompanyName);
         etmobileNumber = findViewById(R.id.etMobileNum);
@@ -79,17 +72,19 @@ public class AddCardActivity extends AppCompatActivity {
         etEmail = findViewById(R.id.etEmail);
         etwebSite = findViewById(R.id.etWebSite);
         etaddressLine1 = findViewById(R.id.etAddLine1);
-        etadressLine2 = findViewById(R.id.etAddLine2);
+        etaddressLine2 = findViewById(R.id.etAddLine2);
 
         buttonSaveCard = findViewById(R.id.btnSaveCard);
         buttonReScanCard = findViewById(R.id.btnReScan);
 
         base64file = CameraActivity.resizedImage;
 
+        sendCard();
+
         ivCloseCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -98,24 +93,43 @@ public class AddCardActivity extends AppCompatActivity {
         ivbackCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),CameraActivity.class);
+                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
-        sendCard();
+        buttonSaveCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!etUserName.getText().toString().isEmpty() && (!etmobileNumber.getText().toString().isEmpty() || !etotherMobileNumber.getText().toString().isEmpty())) {
+                    savecard();
+//                    Toast.makeText(getApplicationContext(), "Contact Saved!", Toast.LENGTH_SHORT).show();
+                    saveButtonShowPopup();
+                    clearfileds();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Name and Contact Number Can not " +
+                            "Empty!", Toast.LENGTH_SHORT).show();
+                }
 
-
-//        buttonSaveCard.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-
+            }
+        });
     }
 
+    private void clearfileds() {
+        etUserName.setText(null);
+        etTitle.setText(null);
+        etcompanyName.setText(null);
+        etmobileNumber.setText(null);
+        etotherMobileNumber.setText(null);
+        etEmail.setText(null);
+        etwebSite.setText(null);
+        etaddressLine1.setText(null);
+        etaddressLine2.setText(null);
+    }
 
     private void sendCard() {
         AppProgressDialog.showProgressDialog(this, "Scanning..", false);
@@ -157,7 +171,7 @@ public class AddCardActivity extends AppCompatActivity {
                         if (numArray.length() > 0) {
                             String mobileNum1 = numArray.getString(0);
                             etmobileNumber.setText(mobileNum1);
-                            Log.d("ssss", "ssss" + mobileNum1);
+//                            Log.d("ssss", "ssss" + mobileNum1);
 
                         }
                         if (numArray.length() > 1) {
@@ -194,6 +208,82 @@ public class AddCardActivity extends AppCompatActivity {
                         if (response.data.length == 0) {
                             byte[] responseData = "{}".getBytes("UTF8");
                             response = new NetworkResponse(response.statusCode, responseData, response.headers, response.notModified);
+//                            Log.d("ddddddddddddddddd", "cccc " + responseData.toString());
+                        }
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    return super.parseNetworkResponse(response);
+                }
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+                String token = pref.getString("token", "");
+                params.put("Authorization", "Bearer " + token);
+                return params;
+            }
+        };
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjReq);
+    }
+
+    private void savecard() {
+        card_type = 1;
+
+        AppProgressDialog.showProgressDialog(this, "Saving..", false);
+        String url = "http://thechaptersrilanka.com/sky_card_backend/public/api/auth/save_cards";
+        JSONObject obj = new JSONObject();
+
+        try {
+            obj.put("name", etUserName.getText().toString());
+            obj.put("title", etTitle.getText().toString());
+            obj.put("email", etEmail.getText().toString());
+            obj.put("address_line1", etaddressLine1.getText().toString());
+            obj.put("address_line2", etaddressLine2.getText().toString());
+            obj.put("phone_number1", etmobileNumber.getText().toString());
+            obj.put("phone_number2", etotherMobileNumber.getText().toString());
+            obj.put("website", etwebSite.getText().toString());
+            obj.put("card_type", card_type);
+            obj.put("card_image", "abcd");
+            obj.put("company_name", etcompanyName.getText().toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                url, obj, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                AppProgressDialog.hideProgressDialog();
+//                Log.d("ddddddd", "ffffffffffffffffffffff");
+                Log.d("ssssss", "sssss " + response);
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("resposneerror ", "error ");
+                error.printStackTrace();
+                AppProgressDialog.hideProgressDialog();
+            }
+
+        }) {
+            @Override
+            protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+                Log.d("dddddddddddddddd", "network Response " + response.statusCode);
+                if (response.statusCode != 200) {
+                    return Response.error(new ParseError(response));
+                } else {
+                    try {
+                        if (response.data.length == 0) {
+                            byte[] responseData = "{}".getBytes("UTF8");
+                            response = new NetworkResponse(response.statusCode, responseData, response.headers, response.notModified);
                             Log.d("ddddddddddddddddd", "cccc " + responseData.toString());
                         }
                     } catch (UnsupportedEncodingException e) {
@@ -218,6 +308,34 @@ public class AddCardActivity extends AppCompatActivity {
         queue.add(jsonObjReq);
     }
 
+    public void saveButtonShowPopup() {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.save_popup_window, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+//        popupWindow.showAtLocation(view, Gravity.CENTER, 10, 20);
+        popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
 
 }
 
--- SkycardApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ SkycardApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,19 +1,14 @@
 package com.example.myapplication;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,11 +20,26 @@ import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.example.myapplication.other.CircleTransform;
+import com.example.myapplication.helper.AppProgressDialog;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,9 +48,11 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawer;
     private CardView cvAddCard, cvCardHolder, cvMyCards, cvSkyDaily, cvSkyNetwork, cvShareCards,
             cvSkyVip, cvSkySmiles, cvSkyChats;
-
-
+    private ImageView profilePic;
     private Toolbar toolbar;
+    private TextView tvFullName;
+    private View navHeader;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +60,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.botom_navigation);
+
         topNavigationView = findViewById(R.id.nav_view);
+        navHeader = topNavigationView.getHeaderView(0);
+        profilePic = navHeader.findViewById(R.id.ivProfilePic);
+        tvFullName = navHeader.findViewById(R.id.tvProfileName);
+
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
@@ -71,6 +88,8 @@ public class MainActivity extends AppCompatActivity {
                 R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        requestprofiledetails();
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -106,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
                 if (item.getItemId() == R.id.naviAddcard) {
-                    Intent intent = new Intent(getApplicationContext(), AddCardsActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
                     startActivity(intent);
 
                 }
@@ -152,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
         cvAddCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), AddCardsActivity.class);
+                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
                 startActivity(intent);
 
             }
@@ -206,14 +225,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
             }
         });
 
         cvSkySmiles.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
 
             }
         });
@@ -222,13 +239,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
             }
         });
 
-
     }
-
 
     @Override
     public void onBackPressed() {
@@ -253,20 +267,119 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_bell,menu);
+        inflater.inflate(R.menu.menu_bell, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected( MenuItem item) {
-       switch (item.getItemId()){
-           case  R.id.bell:
-               Intent intent = new Intent(getApplicationContext(),NotificationsActivity.class);
-               startActivity(intent);
-               return true;
-           default:
-               return super.onOptionsItemSelected(item);
-       }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.bell:
+                Intent intent = new Intent(getApplicationContext(), NotificationsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
+    }
+
+    public void requestprofiledetails() {
+
+        String url = "http://thechaptersrilanka.com/sky_card_backend/public/api/auth/userDetails";
+        JSONObject obj = new JSONObject();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                url, obj, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                AppProgressDialog.hideProgressDialog();
+//                Log.d("ddddddd", "ffffffffffffffffffffff");
+//                Log.d("ddddddd", "ccccc " + response);
+
+                try {
+                    if (!response.isNull("profile_picture")) {
+//                        String profilepicture = response.getString("profile_picture");
+                        Glide.with(MainActivity.this).load("profile_picture").into(profilePic);
+//                        profilePic.setImageResource(Integer.parseInt(profilepicture));
+                    }
+
+//                    if (!response.isNull("email")) {
+//                        JSONArray emailArray = response.getJSONArray("email");
+//                        if (emailArray.length() > 0) {
+//                            String email = emailArray.getString(0);
+//                            etEmail.setText(email);
+//                        }
+//                    }
+
+                    if (!response.isNull("full_name")) {
+                        String fullName = response.getString("full_name");
+                        tvFullName.setText(fullName);
+                    }
+
+
+//                    if (!response.isNull("phone_number")) {
+//                        JSONArray numArray = response.getJSONArray("phone_number");
+//                        if (numArray.length() > 0) {
+//                            String mobileNum1 = numArray.getString(0);
+//                            etmobileNumber.setText(mobileNum1);
+//                            Log.d("ssss", "ssss" + mobileNum1);
+//
+//                        }
+//                        if (numArray.length() > 1) {
+//                            String mobileNum2 = numArray.getString(1);
+//                            etotherMobileNumber.setText(mobileNum2);
+//                        }
+//                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("resposneerror ", "error ");
+                error.printStackTrace();
+//                Toast.makeText(getApplicationContext(),
+//                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        }) {
+            @Override
+            protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+                Log.d("dddddddddddddddd", "error 2 " + response.statusCode);
+                if (response.statusCode != 200) {
+                    return Response.error(new ParseError(response));
+                } else {
+                    try {
+                        if (response.data.length == 0) {
+                            byte[] responseData = "{}".getBytes("UTF8");
+                            response = new NetworkResponse(response.statusCode, responseData, response.headers, response.notModified);
+                            Log.d("ddddddddddddddddd", "cccc " + responseData.toString());
+                        }
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    return super.parseNetworkResponse(response);
+                }
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+                String token = pref.getString("token", "");
+                params.put("Authorization", "Bearer " + token);
+                return params;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjReq);
     }
 }
